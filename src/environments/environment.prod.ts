09ed62a02b84920374e6450ad54f270ca1beeae4
JohnGriparis <john.griparis@gmail.com>
2021-02-10 18:44:15 +0200

    test done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.1.149:3000'
+  apiUrl: 'https://fusion2.ornico.co.za'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.1.149:3000'
+  apiUrl: 'https://fusion2.ornico.co.za'
 };
